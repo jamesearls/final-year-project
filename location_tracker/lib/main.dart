6e5eaec8d428acbd1c142c27f76aa89f2eb4c737
40206210 <jearls01@qub.ac.uk>
2022-01-19 16:31:41 +0000

    fixed main.dart file

--- location_tracker/lib/main.dart
+++ location_tracker/lib/main.dart
@@ -1,11 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-
-Future main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-
 import 'app.dart';
 
 Future main() async {
