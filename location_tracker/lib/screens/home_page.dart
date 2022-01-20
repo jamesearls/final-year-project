538a2f157463ec3c25811665218ec50d75db84ae
40206210 <jearls01@qub.ac.uk>
2022-01-20 18:09:21 +0000

    commented out home page

--- location_tracker/lib/screens/home_page.dart
+++ location_tracker/lib/screens/home_page.dart
@@ -1,62 +1,62 @@
-import 'package:flutter/material.dart';
-import 'package:geolocator/geolocator.dart';
+// import 'package:flutter/material.dart';
+// import 'package:geolocator/geolocator.dart';
 
-class HomePage extends StatefulWidget {
-  @override
-  _HomePageState createState() => _HomePageState();
-}
+// class HomePage extends StatefulWidget {
+//   @override
+//   _HomePageState createState() => _HomePageState();
+// }
 
-class _HomePageState extends State<HomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Location"),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            if (_currentAddress != null) Text(_currentAddress),
-            FlatButton(
-              child: Text("Get location"),
-              onPressed: () {
-                _getCurrentLocation();
-              },
-            ),
-          ],
-        ),
-      ),
-    );
-  }
+// class _HomePageState extends State<HomePage> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text("Location"),
+//       ),
+//       body: Center(
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: <Widget>[
+//             if (_currentAddress != null) Text(_currentAddress),
+//             FlatButton(
+//               child: Text("Get location"),
+//               onPressed: () {
+//                 _getCurrentLocation();
+//               },
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
 
-  _getCurrentLocation() {
-    Geolocator.getCurrentPosition(
-            desiredAccuracy: LocationAccuracy.best,
-            forceAndroidLocationManager: true)
-        .then((Position position) {
-      setState(() {
-        _currentPosition = position;
-        _getAddressFromLatLng();
-      });
-    }).catchError((e) {
-      print(e);
-    });
-  }
+//   _getCurrentLocation() {
+//     Geolocator.getCurrentPosition(
+//             desiredAccuracy: LocationAccuracy.best,
+//             forceAndroidLocationManager: true)
+//         .then((Position position) {
+//       setState(() {
+//         _currentPosition = position;
+//         _getAddressFromLatLng();
+//       });
+//     }).catchError((e) {
+//       print(e);
+//     });
+//   }
 
-  _getAddressFromLatLng() async {
-    try {
-      List<Placemark> placemarks = await placemarkFromCoordinates(
-          _currentPosition.latitude, _currentPosition.longitude);
+//   _getAddressFromLatLng() async {
+//     try {
+//       List<Placemark> placemarks = await placemarkFromCoordinates(
+//           _currentPosition.latitude, _currentPosition.longitude);
 
-      Placemark place = placemarks[0];
+//       Placemark place = placemarks[0];
 
-      setState(() {
-        _currentAddress =
-            "${place.locality}, ${place.postalCode}, ${place.country}";
-      });
-    } catch (e) {
-      print(e);
-    }
-  }
-}
+//       setState(() {
+//         _currentAddress =
+//             "${place.locality}, ${place.postalCode}, ${place.country}";
+//       });
+//     } catch (e) {
+//       print(e);
+//     }
+//   }
+// }
