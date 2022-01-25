79896d6f76efa1c4a39047be07fe3a40641b2ffe
40206210 <jearls01@qub.ac.uk>
2022-01-25 17:06:06 +0000

    Added files to be built upon later

--- location_tracker/test/widget_test.dart
+++ location_tracker/test/widget_test.dart
@@ -9,8 +9,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:location_tracker/app.dart';
 
-import 'package:location_tracker/main%202.dart';
-
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
