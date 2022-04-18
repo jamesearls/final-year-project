32ccfc1976039d7addaf5403fdd78559d88344c1
40206210 <jearls01@qub.ac.uk>
2022-04-18 18:42:51 +0100

    added specific theme.dart file

---
+++ location_tracker/lib/theme.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+var appTheme = ThemeData(
+  fontFamily: GoogleFonts.nunito().fontFamily,
+  brightness: Brightness.dark,
+  primarySwatch: Colors.deepPurple,
+  primaryColor: Colors.blue,
+  primaryColorDark: Colors.blue,
+  textTheme: const TextTheme(
+      bodyText1: TextStyle(fontSize: 18),
+      bodyText2: TextStyle(fontSize: 16),
+      button: TextStyle(
+        fontWeight: FontWeight.bold,
+      )),
+);
