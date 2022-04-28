516cbca994a2d244dc3182d1ec5ae12039f5a6a3
40206210 <jearls01@qub.ac.uk>
2022-04-29 00:42:01 +0100

    simplified screen imports

--- location_tracker/lib/routes.dart
+++ location_tracker/lib/routes.dart
@@ -1,10 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:location_tracker/screens/home_screen/home_screen.dart';
-import 'package:location_tracker/screens/login_screen/login_screen.dart';
-import 'package:location_tracker/screens/register_screen/register_screen.dart';
-import 'package:location_tracker/screens/profile_screen/profile_screen.dart';
-import 'package:location_tracker/screens/map_screen/map_screen.dart';
-import 'package:location_tracker/screens/index_screen/index_screen.dart';
+import 'screens/screens.dart';
 
 late final User user;
 var appRoutes = {
@@ -14,4 +9,5 @@ var appRoutes = {
   // '/register': (context) => RegisterScreen(),
   '/profile': (context) => const ProfileScreen(),
   '/map': (context) => const MapScreen(),
+  '/admin': (context) => const AdminScreen(),
 };
