0ef8ba87544bd57f31305ae505a3a8a6c0a372e9
40206210 <jearls01@qub.ac.uk>
2022-04-28 22:15:49 +0100

    Cleanup Cloud Functions added

--- firebase_functions/functions/src/index.ts
+++ firebase_functions/functions/src/index.ts
@@ -1,9 +1,72 @@
 import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
+admin.initializeApp();
+const db = admin.firestore();
 
-// Start writing Firebase Functions
-// https://firebase.google.com/docs/functions/typescript
+export const CleanAnonUsers = functions
+    .region("europe-west2")
+    .pubsub
+    .schedule("0 3 * * *")
+    .timeZone("Europe/Belfast")
+    .onRun(() => {
+      // deletes all the anon users in the Authentication
+      admin
+          .auth()
+          .listUsers(50)
+          .then(function(listUsersResult) {
+            listUsersResult.users.forEach(function(userRecord) {
+              if (userRecord.providerData.length === 0) {
+                admin.auth().deleteUser(userRecord.uid)
+                    .then(function() {
+                      console.log("Successfully deleted user");
+                    })
+                    .catch(function(error) {
+                      console.log("Error deleting user:", error);
+                    });
+              }
+            });
+          })
+          .catch(function(error) {
+            console.log("Error listing users:", error);
+          });
 
-export const helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
-});
+      // deletes all the anon users in the firestore
+      const userQuery = db
+          .collection("users")
+          .where("displayName", "==", "guest");
+      userQuery.get().then(function(querySnapshot) {
+        querySnapshot.forEach(function(doc) {
+          doc.ref.delete();
+        });
+      });
+    });
+
+export const cleanUsersInBuildings = functions
+    .region("europe-west2")
+    .pubsub
+    .schedule("0 3 * * *")
+    .timeZone("Europe/Belfast")
+    .onRun(() => {
+      db
+          .collection("usersInBuildings")
+          .listDocuments().then((val) => {
+            val.map((val) => {
+              val.delete();
+            });
+          });
+    });
+
+export const cleanUsersInRooms = functions
+    .region("europe-west2")
+    .pubsub
+    .schedule("0 3 * * *")
+    .timeZone("Europe/Belfast")
+    .onRun(() => {
+      db
+          .collection("usersInRooms")
+          .listDocuments().then((val) => {
+            val.map((val) => {
+              val.delete();
+            });
+          });
+    });
