7dc54f49164641e301d94509992b372093d3ad87
40206210 <jearls01@qub.ac.uk>
2022-04-29 00:45:24 +0100

    Added Admin Button

---
+++ location_tracker/lib/screens/admin/admin_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class AdminScreen extends StatelessWidget {
+  const AdminScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Admin View'),
+      ),
+      body: Center(
+        child: Text('Admin'),
+      ),
+    );
+  }
+}
---
+++ location_tracker/lib/shared/admin_button.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:location_tracker/models/models.dart';
+import 'package:provider/provider.dart';
+
+class AdminButton extends StatelessWidget {
+  const AdminButton({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    User userModel = Provider.of<User>(context);
+
+    if (userModel.isAdmin) {
+      return Align(
+        alignment: Alignment.bottomRight,
+        child: Container(
+          padding: const EdgeInsets.all(8),
+          child: FloatingActionButton.extended(
+            onPressed: () {
+              Navigator.pushNamed(context, '/admin');
+            },
+            icon: const Icon(
+              FontAwesomeIcons.userShield,
+              color: Colors.white,
+            ),
+            label: const Text('Admin'),
+          ),
+        ),
+      );
+    }
+    return Container();
+  }
+}
